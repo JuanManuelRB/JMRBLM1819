590e474ef0edbca63bcbbd4658222319fa0897c3
Alumno <jumarobo982013@gmail.com>
2019-03-11 14:42:11 +0100

    new

---
+++ PHP/ejercicio13/insercion.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+    <div class="form-group">
+        <label for="nombre">Email address</label>
+        <input type="text" class="form-control" id="nombre" name="nombre" aria-describedby="emailHelp" placeholder="Enter name">
+    </div>
+    <div class="form-group">
+        <label for="email">Email address</label>
+        <input type="text" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter name">
+    </div>
+    <div class="form-group">
+        <label for="curso">Email address</label>
+        <input type="text" class="form-control" id="curso" name="curso" aria-describedby="emailHelp" placeholder="Enter name">
+    </div>
+    <div class="form-group form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</body>
+</html>
