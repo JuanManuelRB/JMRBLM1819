849077c55e5612ea8a8540f9d94ca60edd95c204
Alumno <jumarobo982013@gmail.com>
2019-03-28 13:42:30 +0100

    zsdf

--- PHP/MET_DB/index.php
+++ PHP/MET_DB/index.php
@@ -10,7 +10,7 @@
 <body>
 
 <div class="container">
-    <form action="1.php" method="get">
+    <form action="index.php" method="post">
         <div class="form-group">
             <label for="FI">Fecha Inicio</label>
             <input type="date" class="form-control" id="FI" name="FI" placeholder="Ingresa una fecha">
@@ -20,21 +20,43 @@
             <input type="date" class="form-control" id="FF" name="FF" placeholder="Ingresa una fecha">
         </div>
         <div class="form-group">
-                <label for="Tipo">Example select</label>
+                <label for="Tipo">Tipo</label>
                 <select class="form-control" id="Tipo" name="Tipo">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
+                    <option value="">Tipo 1</option>
+                    <option value="">Tipo 2</option>
+                    <option value="">Tipo 3</option>
+                    <option value="">Tipo 4</option>
+                    <option value="">Tipo 5</option>
                 </select>
+        </div>
+        <div class="form-group">
+            <label for="Valor">Valor</label>
+            <input type="date" class="form-control" id="Valor" name="Valor">
         </div>
             <button type="submit" class="btn btn-primary">Calcular</button>
     </form>
     </div>
 
     <div>
-        <table></table>
+        <table>
+
+        <?php
+            $fechaInicio = $_REQUEST['FI'];
+            $fechaFin = $_REQUEST['FF'];
+            $tipo = $_REQUEST['Tipo'];
+            $valor = $_REQUEST['Valor'];
+
+            function consulta($inicio, $fin, $tipo, $valor){
+                $a = toString($inicio) + toString($fin)
+                return 
+            }
+
+            $conn = mysqli_connect("localhost", "root", "", "estacion") or die("Problema en la conexión");
+            $reg = mysqli_query($conn, "SELECT limit ,5"
+                                ) or die("Error en la consulta: ".mysqli_error($conn));
+        ?>
+
+        </table>
     </div>
 
     <form action="1.php" method="get">
